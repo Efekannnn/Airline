05ba690a6334aec6fe73bd2f1b23debad8b06262
Efekan Tosmak <128170914+Efekannnn@users.noreply.github.com>
2024-05-04 14:42:27 +0300

    Delete airlines directory

--- airlines/havayolları/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views 
-
-urlpatterns = [
-    path('',views.anasayfa),
-    path('search/', views.flight_search, name='flight_search'),  
-    path('bilet-al/', views.bilet_al, name='bilet_al'),
-
-]
