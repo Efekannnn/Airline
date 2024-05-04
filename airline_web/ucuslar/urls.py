844bcca4a16e9cfcf903c79dbb97084bff70c196
Efekan Tosmak <128170914+Efekannnn@users.noreply.github.com>
2024-05-04 14:39:19 +0300

    Delete airline_web directory

--- airline_web/ucuslar/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.anasayfa, name='index'),
-    path('/list', views.list_flight, name='list'),
-    path('/<int:flight_id>', views.flight, name="flight")
-]
