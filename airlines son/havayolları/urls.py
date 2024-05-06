cc4bfdb3a0a4c9439dd5814f83d9430f1fbd87ce
Efekan Tosmak <tsmkefekan05@outlook.com>
2024-05-06 13:31:00 +0300

    ad değişikliği

--- airlines son/havayolları/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views 
-
-urlpatterns = [
-    path('',views.anasayfa),
-    path('search/', views.flight_search, name='flight_search'),  
-    path('bilet-kaydet/', views.bilet_kaydet, name='bilet_kaydet'),  
-]
--- airlines son/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
- path('iletisim/', views.iletisim, name='iletisim'),
- path('submit', views.submit_form, name='submit_form'),
- path('hakkimizda',views.hakkimizda,name='hakkimizda'),
-
-]
